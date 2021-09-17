3dcd4c64a1a0802ade2017537f1844c86b4b9b26
yajat10 <88446514+yajat10@users.noreply.github.com>
2021-09-17 17:28:44 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,5 @@
+function log_in(){
+    username=document.getElementById("username").value;
+    localStorage.setItem("username",username);
+    window.location="kwitter_room.html"
+}
---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCShQyNoaKHVXnt7W09Ob781CsrI5YSgs8",
+    authDomain: "kwitter-f1a40.firebaseapp.com",
+    databaseURL: "https://kwitter-f1a40-default-rtdb.firebaseio.com",
+    projectId: "kwitter-f1a40",
+    storageBucket: "kwitter-f1a40.appspot.com",
+    messagingSenderId: "108516170241",
+    appId: "1:108516170241:web:36e2ddb29e5e85d48a7e00"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
